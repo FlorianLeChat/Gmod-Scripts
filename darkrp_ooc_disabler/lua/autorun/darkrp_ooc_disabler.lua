730e00cdb35c85004a8c2288e469f3baa1aa1262
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-25 14:57:23 +0200

    OOC disabler code improvements

--- darkrp_ooc_disabler/lua/autorun/darkrp_ooc_disabler.lua
+++ darkrp_ooc_disabler/lua/autorun/darkrp_ooc_disabler.lua
@@ -1,91 +1,121 @@
 -- [[ SETTINGS ]] --
 
--- What the name of this command ? (Default : "ooccontrol")
+-- What the name of this command? (default : "ooccontrol")
 local command_name = "ooccontrol"
--- What is the delay for DarkRP anti-spam command ? (Default : 1.5)
+-- What is the delay for DarkRP anti-spam command? (default : 1.5)
 local command_delay = 1.5
--- Admins can always use ooc when is disabled ? (Default : true)
+-- Admins can always use ooc when is disabled? (default : true)
 local admin_can_always_use_ooc = true
 
 -- [[ CODE ]] --
 
-function OOC_Disabler_GamemodeCheck()
-	if DarkRP then
-		if SERVER then
+hook.Add("PostGamemodeLoaded", "Gmod.Workshop.DarkRP_OOC_Disabler", function()
 
-			-- OOC CONTROL COMMAND
+	-- Gamemode check
+	if not DarkRP then
 
-			local function OOC_CONTROL(ply)
-				local pname = ply:Nick()
+		timer.Create("Gmod.Workshop.DarkRP_OOC_Disabler", 30, 0, function()
 
-				if ply:IsAdmin() then
-					if GAMEMODE.Config.ooc then
-						GAMEMODE.Config.ooc = false
-						DarkRP.notifyAll(0, 5, "OOC was disabled by " .. pname .. " !")
-					else
-						GAMEMODE.Config.ooc = true
-						DarkRP.notifyAll(0, 5, "OOC was enabled by " .. pname .. " !")
-					end
+			local players = player.GetAll()
+
+			for i = 1, #players do
+
+				players[i]:ChatPrint("The addon \"[DarkRP] OOC Disabler\" didn't load because it works only on the DarkRP or derived gamemode.")
+
+			end
+
+		end)
+
+		return
+
+	end
+
+	if SERVER then
+
+		-- OOC CONTROL COMMAND
+
+		local function OOC_CONTROL(ply, args)
+
+			if ply:IsAdmin() then
+
+				local playerName = ply:Nick()
+
+				if GAMEMODE.Config.ooc then
+
+					DarkRP.notifyAll(0, 5, "The OOC channel has been disabled by " .. playerName .. ".")
+
 				else
-					DarkRP.notify("You're not authorized to execute this command.")
+
+					DarkRP.notifyAll(0, 5, "The OOC channel has been enabled by " .. playerName .. ".")
+
 				end
 
-				return ""
+				GAMEMODE.Config.ooc = not GAMEMODE.Config.ooc
+
+			else
+
+				DarkRP.notify("You are not authorized to execute this command.")
+
 			end
-			DarkRP.defineChatCommand(command_name, OOC_CONTROL)
 
-			-- OOC COMMAND MODIFICATION
+			return ""
 
-			if admin_can_always_use_ooc then
-				local function OOC(ply, args)
-					if not GAMEMODE.Config.ooc and not ply:IsAdmin() then
-						DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("disabled", DarkRP.getPhrase("ooc"), ""))
+		end
+		DarkRP.defineChatCommand(command_name, OOC_CONTROL)
+
+		-- OOC COMMAND MODIFICATION
+
+		if admin_can_always_use_ooc then
+
+			local function OOC(ply, args)
+
+				-- https://github.com/FPtje/DarkRP/blob/7e06296120f4b6891fc65fd325018e3391a603e4/gamemode/modules/chat/sv_chatcommands.lua#L85-L113
+				if not GAMEMODE.Config.ooc and not ply:IsAdmin() then
+					DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("disabled", DarkRP.getPhrase("ooc"), ""))
+					return ""
+				end
+
+				local DoSay = function(text)
+
+					if text == "" then
+						DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", DarkRP.getPhrase("arguments"), ""))
 						return ""
 					end
 
-					local DoSay = function(text)
-						if text == "" then
-							DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
-							return ""
-						end
-						local col = team.GetColor(ply:Team())
-						local col2 = Color(255, 255, 255, 255)
-						if not ply:Alive() then
-							col2 = Color(255, 200, 200, 255)
-							col = col2
-						end
-
-						local phrase = DarkRP.getPhrase("ooc")
-						local name = ply:Nick()
-						for _, v in ipairs(player.GetAll()) do
-							DarkRP.talkToPerson(v, col, "(" .. phrase .. ") " .. name, col2, text, ply)
-						end
+					local col = team.GetColor(ply:Team())
+					local col2 = Color(255, 255, 255, 255)
+					if not ply:Alive() then
+						col2 = Color(255, 200, 200, 255)
+						col = col2
 					end
-					return args, DoSay
+
+					local phrase = DarkRP.getPhrase("ooc")
+					local name = ply:Nick()
+					for _, v in ipairs(player.GetAll()) do
+						DarkRP.talkToPerson(v, col, "(" .. phrase .. ") " .. name, col2, text, ply)
+					end
+
 				end
-				DarkRP.defineChatCommand("/", OOC, true, 1.5)
-				DarkRP.defineChatCommand("a", OOC, true, 1.5)
-				DarkRP.defineChatCommand("ooc", OOC, true, 1.5)
+
+				return args, DoSay
+
 			end
+			DarkRP.defineChatCommand("/", OOC)
+			DarkRP.defineChatCommand("a", OOC)
+			DarkRP.defineChatCommand("ooc", OOC)
+
 		end
 
-		DarkRP.declareChatCommand{
-			command = command_name,
-			description = "Enable or disable OOC.",
-			delay = command_delay
-		}
-	else
-		timer.Create("OOC_Disabler.GamemodeInitError", 30, 0, function()
-			for _, v in ipairs(player.GetAll()) do
-				v:ChatPrint("'OOC Disabler' was not loaded because he only work with DarkRP or derived !")
-			end
-		end)
 	end
-end
-hook.Add("OnGamemodeLoaded", "OOC_Disabler_GamemodeCheck", OOC_Disabler_GamemodeCheck)
 
-concommand.Add("oocdisabler_check", function(ply)
-	ply:ChatPrint("This server use 'OOC Disabler' script ! ( https://steamcommunity.com/sharedfiles/filedetails/?id=1314167955 )")
+	DarkRP.declareChatCommand({
+
+		command = command_name,
+		description = "Allows you to disable or re-enable the OOC channel.",
+		delay = command_delay
+
+	})
+
 end)
 
 -- Made by "The Cat" alias "Florian #" ( http://steamcommunity.com/profiles/76561198053479101 )
