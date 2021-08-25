283535120de00a476984b8832fe726e025b1640b
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-25 14:27:48 +0200

    Add "DarkRP rpname restrictions" addon

---
+++ darkrp_rpname/lua/darkrp_modules/darkrp_rpname/sv_darkrp_rpname.lua
@@ -0,0 +1,33 @@
+-- https://github.com/FPtje/DarkRP/blob/61d689ded48341b7d9f5f4a303161c3731e7f633/gamemode/modules/base/sv_gamemode_functions.lua#L29-L37
+local blacklist = {
+	["ooc"] = true,
+	["shared"] = true,
+	["world"] = true,
+	["world prop"] = true
+}
+
+GAMEMODE.CanChangeRPName = function() end
+
+hook.Add("CanChangeRPName", "FScript.Workshop.DarkRP_Names", function(ply, name)
+
+	if blacklist[string.lower(name)] then
+
+		return false, DarkRP.getPhrase("forbidden_name")
+
+	end
+
+	local length = #name
+
+	if length > 35 then
+
+		return false, DarkRP.getPhrase("too_long")
+
+	end
+
+	if length < 3 then
+
+		return false, DarkRP.getPhrase("too_short")
+
+	end
+
+end)
