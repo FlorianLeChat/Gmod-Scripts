91e33c3123f7ee1d6dccafc4ccd9aaa55dc10e40
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-25 14:23:31 +0200

    Add "Death Report" addon

---
+++ death_report/lua/autorun/server/death_report.lua
@@ -0,0 +1,8 @@
+util.AddNetworkString("DeathReport")
+
+hook.Add("PlayerDeath", "DeathReport", function(victim, inflictor, attacker)
+	if victim != attacker and victim:IsPlayer() and attacker:IsPlayer() then
+		net.Start("DeathReport")
+		net.Send(victim)
+	end
+end)
