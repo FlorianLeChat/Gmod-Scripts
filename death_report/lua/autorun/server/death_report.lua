78dfaae29a213691a3df0107934d30f2f24bb257
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-25 14:24:16 +0200

    Replace some Lua operator in death report addon

--- death_report/lua/autorun/server/death_report.lua
+++ death_report/lua/autorun/server/death_report.lua
@@ -1,7 +1,7 @@
 util.AddNetworkString("DeathReport")
 
 hook.Add("PlayerDeath", "DeathReport", function(victim, inflictor, attacker)
-	if victim != attacker and victim:IsPlayer() and attacker:IsPlayer() then
+	if victim ~= attacker and victim:IsPlayer() and attacker:IsPlayer() then
 		net.Start("DeathReport")
 		net.Send(victim)
 	end
